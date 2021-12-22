92a003d415f0128d3d95ad904dfe79f79cb4fddf
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2021-12-22 17:17:16 +0100

    rekenmachine in C#

---
+++ HuiswerkRekenmachine/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace HuiswerkRekenmachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnKeer = new System.Windows.Forms.Button();
+            this.btnDelen = new System.Windows.Forms.Button();
+            this.btnKomma = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnIs = new System.Windows.Forms.Button();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAntwoord = new System.Windows.Forms.TextBox();
+            this.lblAntwoord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(70, 73);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 66);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(70, 145);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 66);
+            this.btn4.TabIndex = 1;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(70, 217);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 66);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(151, 73);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 66);
+            this.btn8.TabIndex = 3;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(151, 145);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 66);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(151, 217);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 66);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(232, 73);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 66);
+            this.btn9.TabIndex = 6;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(232, 145);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 66);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(232, 217);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 66);
+            this.btn3.TabIndex = 8;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.NumEvent);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(313, 217);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(75, 66);
+            this.btnMin.TabIndex = 9;
+            this.btnMin.Text = "-";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.OperandEvent);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(313, 289);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 66);
+            this.btnPlus.TabIndex = 10;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.OperandEvent);
+            // 
+            // btnKeer
+            // 
+            this.btnKeer.Location = new System.Drawing.Point(313, 145);
+            this.btnKeer.Name = "btnKeer";
+            this.btnKeer.Size = new System.Drawing.Size(75, 66);
+            this.btnKeer.TabIndex = 11;
+            this.btnKeer.Text = "*";
+            this.btnKeer.UseVisualStyleBackColor = true;
+            this.btnKeer.Click += new System.EventHandler(this.OperandEvent);
+            // 
+            // btnDelen
+            // 
+            this.btnDelen.Location = new System.Drawing.Point(313, 73);
+            this.btnDelen.Name = "btnDelen";
+            this.btnDelen.Size = new System.Drawing.Size(75, 66);
+            this.btnDelen.TabIndex = 12;
+            this.btnDelen.Text = "/";
+            this.btnDelen.UseVisualStyleBackColor = true;
+            this.btnDelen.Click += new System.EventHandler(this.OperandEvent);
+            // 
+            // btnKomma
+            // 
+            this.btnKomma.Location = new System.Drawing.Point(232, 289);
+            this.btnKomma.Name = "btnKomma";
+            this.btnKomma.Size = new System.Drawing.Size(75, 66);
+            this.btnKomma.TabIndex = 13;
+            this.btnKomma.Text = ",";
+            this.btnKomma.UseVisualStyleBackColor = true;
+            this.btnKomma.Click += new System.EventHandler(this.btnKomma_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(70, 289);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(156, 66);
+            this.btn0.TabIndex = 14;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(394, 145);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 66);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "C";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnIs
+            // 
+            this.btnIs.Location = new System.Drawing.Point(394, 217);
+            this.btnIs.Name = "btnIs";
+            this.btnIs.Size = new System.Drawing.Size(75, 138);
+            this.btnIs.TabIndex = 16;
+            this.btnIs.Text = "=";
+            this.btnIs.UseVisualStyleBackColor = true;
+            this.btnIs.Click += new System.EventHandler(this.btnIs_Click);
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Location = new System.Drawing.Point(394, 73);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(75, 66);
+            this.btnDeleteAll.TabIndex = 17;
+            this.btnDeleteAll.Text = "CE";
+            this.btnDeleteAll.UseVisualStyleBackColor = true;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 18;
+            // 
+            // txtAntwoord
+            // 
+            this.txtAntwoord.Location = new System.Drawing.Point(70, 45);
+            this.txtAntwoord.Name = "txtAntwoord";
+            this.txtAntwoord.Size = new System.Drawing.Size(318, 22);
+            this.txtAntwoord.TabIndex = 19;
+            // 
+            // lblAntwoord
+            // 
+            this.lblAntwoord.AutoSize = true;
+            this.lblAntwoord.Location = new System.Drawing.Point(70, 13);
+            this.lblAntwoord.Name = "lblAntwoord";
+            this.lblAntwoord.Size = new System.Drawing.Size(0, 17);
+            this.lblAntwoord.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAntwoord);
+            this.Controls.Add(this.txtAntwoord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.btnIs);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnKomma);
+            this.Controls.Add(this.btnDelen);
+            this.Controls.Add(this.btnKeer);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn7);
+            this.Name = "Form1";
+            this.Text = "Huiswerk rekenmachine Nick van Loo";
+            this.Click += new System.EventHandler(this.NumEvent);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnKeer;
+        private System.Windows.Forms.Button btnDelen;
+        private System.Windows.Forms.Button btnKomma;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnIs;
+        private System.Windows.Forms.Button btnDeleteAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAntwoord;
+        private System.Windows.Forms.Label lblAntwoord;
+    }
+}
+
---
+++ HuiswerkRekenmachine/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuiswerkRekenmachine
+{
+    public partial class Form1 : Form
+    {
+        bool operandPerformed = false;
+        string operand = "";
+        double result = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NumEvent(object sender, EventArgs e)
+        {
+            if (txtAntwoord.Text == "0" || operandPerformed)
+                txtAntwoord.Clear();
+            Button btn = (Button)sender;
+            txtAntwoord.Text += btn.Text;
+            operandPerformed = false;
+        }
+
+        private void OperandEvent(object sender, EventArgs e)
+        {
+            operandPerformed = true;
+            Button btn = (Button)sender;
+            String newOperand = btn.Text;
+
+            lblAntwoord.Text = lblAntwoord.Text + " " + txtAntwoord + " " + newOperand;
+
+            switch(operand)
+            {
+                case "+":txtAntwoord.Text = (result + Double.Parse(txtAntwoord.Text)).ToString();break;
+                case "-": txtAntwoord.Text = (result - Double.Parse(txtAntwoord.Text)).ToString(); break;
+                case "*": txtAntwoord.Text = (result * Double.Parse(txtAntwoord.Text)).ToString(); break;
+                case "/": txtAntwoord.Text = (result / Double.Parse(txtAntwoord.Text)).ToString(); break;
+                default:break;
+            }
+            result = Double.Parse(txtAntwoord.Text);
+            operand = newOperand;
+        }
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            txtAntwoord.Text = "0";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            txtAntwoord.Text = "0";
+            lblAntwoord.Text = "";
+            result = 0;
+            operand = "";
+        }
+
+        private void btnIs_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = "";
+            operandPerformed = true;
+
+            switch (operand)
+            {
+                case "+": txtAntwoord.Text = (result + Double.Parse(txtAntwoord.Text)).ToString(); break;
+                case "-": txtAntwoord.Text = (result - Double.Parse(txtAntwoord.Text)).ToString(); break;
+                case "*": txtAntwoord.Text = (result * Double.Parse(txtAntwoord.Text)).ToString(); break;
+                case "/": txtAntwoord.Text = (result / Double.Parse(txtAntwoord.Text)).ToString(); break;
+                default: break;
+            }
+
+            result = Double.Parse(txtAntwoord.Text);
+            txtAntwoord.Text = result.ToString();
+            result = 0;
+            operand = "";
+        }
+
+        private void btnKomma_Click(object sender, EventArgs e)
+        {
+            if (!operandPerformed && !txtAntwoord.Text.Contains(","))
+            {
+                txtAntwoord.Text += ",";
+            }
+            else if (operandPerformed)
+            {
+                txtAntwoord.Text = "0";
+            }
+
+            if (!txtAntwoord.Text.Contains(","))
+            {
+                txtAntwoord.Text += ",";
+            }
+
+            operandPerformed = false;
+        }
+    }
+}
